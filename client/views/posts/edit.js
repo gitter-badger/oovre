f2eace1ca4fb984b8e4582fb402690de8d5d1cb5
Joshua Kidd <kidd.josh.343@gmail.com>
2014-10-30 02:28:07 -0600

    Added images... not working...

--- client/views/posts/edit.js
+++ client/views/posts/edit.js
@@ -1,15 +1,48 @@
 Template.editPost.helpers({
 
-    title: function() {
-        return this.title;
+    title: function() { return this.title; },
+    content: function() { return this.content; },
+    status: function() { return this.status; },
+    image: function() { return Images.findOne(this.imageId); }
+
+});
+
+Template.editPost.events({
+
+    'submit #editPostForm': function(e) {
+        e.preventDefault();
+
+        var image;
+        var currentPostId = this._id;
+        var file = $(e.target).find('[name=files]').files[0];
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            image = Images.insert({src: e.target.result});
+        };
+        reader.readAsDataURL(file);
+
+        var post = {
+            title: $(e.target).find('[name=title]'),
+            content: $(e.target).find('[name=content]'),
+            imageId: image._id
+        };
+
+        Posts.update(currentPostId, {$set: post}, function(error) {
+            // Finish updating or show errors
+        });
     },
 
-    content: function() {
-        return this.content;
-    },
+    'click #publishPost': function(e) {
+        e.preventDefault();
 
-    status: function() {
-        return this.status;
+        var post = {
+            status: 'published'
+        }
+
+        Posts.update(this._id, {$set: post}, function(error) {
+
+        });
     }
 
 });
