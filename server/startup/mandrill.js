fa7c57e536fa02a615ba4f0aeb14c63f6669b4c4
Joshua Kidd <kidd.josh.343@gmail.com>
2014-11-05 17:53:57 -0700

    Added gitignore

--- server/startup/mandrill.js
+++
@@ -1,8 +0,0 @@
-// Setup the Mandrill API configuration.
-
-Meteor.startup(function() {
-    return Meteor.Mandrill.config({
-        username: "kidd.josh.343@gmail.com",
-        key: "jus93i7IVMJ65aLOs6F9eQ"
-    });
-});
